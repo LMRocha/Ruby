ad6fd2a80938eab8141fc4f3fdd411910c217dd3
LMRocha <lauro.mrocha@gmail.com>
2018-02-18 17:09:54 -0300

    ajustes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,6 @@ require 'airborne'
 require 'faker'
 require_relative '../app'
 
-ENV['RACK_ENV'] = 'development'
-
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
